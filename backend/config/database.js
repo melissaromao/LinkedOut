41e4fa9bedeb15f588a0fdc34841175ba095c805
melissaromao <melissarrdias@gmail.com>
2024-11-01 19:04:54 -0300

    Configuração para conexão com banco de dados em config/database.js

---
+++ backend/config/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('linkedout', 'postgres', 'root', {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+});
+
+module.exports = sequelize;
