1ab57b35adcad98d72d33632e527e0a72e7d12d5
melissaromao <melissarrdias@gmail.com>
2024-11-05 09:30:12 -0300

    Adicionando sincronização de tabelas de modelos criados usando Sequelize ORM e incluindo tratamento de erro para garantir que os problemas sejam logados

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,25 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('linkedout', 'postgres', 'root', {
     dialect: 'postgres',
     host: 'localhost',
-    port: 5432,
+    port: 5432
 });
+
+const Usuario = require('../models/Usuario');
+const Freelancer = require('../models/Freelancer');
+const Empresa = require('../models/Empresa');
+const Categoria = require('../models/Categoria');
+const Freela = require('../models/Freela');
+const Candidatura = require('../models/Candidatura');
+const Notificacao = require('../models/Notificacao');
+const Pagamento = require('../models/Pagamento');
+
+(async () => {
+    try {
+        await sequelize.sync({force: false});
+        console.log('Sucesso ao sincronizar tabelas');
+    } catch(error) {
+        console.log('Erro ao sincronizar tabelas', error);
+    }
+})();
 
 module.exports = sequelize;
