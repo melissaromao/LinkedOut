182583fe5ace3da80fbb0eb50fdd5179dea8dafd
melissaromao <melissarrdias@gmail.com>
2024-11-05 09:38:26 -0300

    Adicionando require para importar a lógica de configuração do banco de dados, estabelecendo comunicação com a aplicação

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-const sequelize = require('./config/database');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -7,16 +6,8 @@ app.get('/', (req, res) => {
   res.send('Bem-vindo ao LinkedOut!');
 });
 
-//testando conexão com banco de dados
-app.get('/check-database', async (req, res) => {
-    try {
-        await sequelize.authenticate();
-        res.send('Conectado ao banco de dados com sucesso');
-} catch(error) {
-    res.status(500).send('Erro ao conectar ao banco de dados' +error.message);
-}
-});
-
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+require('./config/database');
