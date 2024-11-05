fbfa82bdf959bf5451a49d1909597847aef2e462
melissaromao <melissarrdias@gmail.com>
2024-11-05 09:16:19 -0300

    Criação do modelo Categoria com Sequelize ORM

---
+++ backend/models/Empresa.js
@@ -0,0 +1,62 @@
+const Sequelize = require('sequelize');
+const database = require('../config/database');
+const Usuario = require('./Usuario');
+const Categoria = require('./Categoria');
+const Freela = require('./Freela');
+
+const Empresa = database.define('empresa', {
+    idUsuario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: Usuario,
+            key: 'idUsuario'
+        }
+    },
+
+    idEmpresa: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    cnpj: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    sobreNos: {
+        type: Sequelize.STRING
+    },
+
+    idCategoria: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Categoria,
+            key: 'idCategoria'
+        }
+    },
+
+    latitude: {
+        type: Sequelize.FLOAT
+    },
+
+    longitude: {
+        type: Sequelize.FLOAT
+    }
+    
+}, {
+    timestamps: false,
+});
+
+Empresa.belongsTo(Usuario, {foreignKey: 'idUsuario'});
+Empresa.hasMany(Freela, {foreignKey: 'idEmpresa'});
+
+module.exports = Empresa;
