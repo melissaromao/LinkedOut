86b0cb395aff4c7cae557650dfddb5157b03f3db
melissaromao <melissarrdias@gmail.com>
2024-11-05 10:42:00 -0300

    Alterando o arquivo de configuração da database para conter apenas a conexão com banco de dados, enquanto a sincronização de tabelas foi movida para server.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,22 +5,4 @@ const sequelize = new Sequelize('linkedout', 'postgres', 'root', {
     port: 5432
 });
 
-const Usuario = require('../models/Usuario');
-const Freelancer = require('../models/Freelancer');
-const Empresa = require('../models/Empresa');
-const Categoria = require('../models/Categoria');
-const Freela = require('../models/Freela');
-const Candidatura = require('../models/Candidatura');
-const Notificacao = require('../models/Notificacao');
-const Pagamento = require('../models/Pagamento');
-
-(async () => {
-    try {
-        await sequelize.sync({force: false});
-        console.log('Sucesso ao sincronizar tabelas');
-    } catch(error) {
-        console.log('Erro ao sincronizar tabelas', error);
-    }
-})();
-
 module.exports = sequelize;
