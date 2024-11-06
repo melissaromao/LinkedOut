cc10cd43fdb61d9521ec55561021bde68dbce990
melissaromao <melissarrdias@gmail.com>
2024-11-06 10:32:48 -0300

    Adicionando authController.js para função de login, com verificação de credenciais e geração de token JWT

---
+++ backend/controllers/authController.js
@@ -0,0 +1,40 @@
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+    login: async (req, res) => {
+        const {email, senha} = req.body;
+    
+        try {
+            const usuario = await Usuario.findOne({where: {email}});
+
+            if (!usuario) {
+                return res.status(404).json({message: 'Usuário não encontrado'});
+            }
+
+            const senhaValida = await bcrypt.compare(senha, usuario.senha);
+            if (!senhaValida) {
+                return res.status(400).json({ message: 'Senha incorreta' });
+            }
+
+
+            const token = jwt.sign (
+                {id: usuario.id, email: usuario.email},
+                process.env.JWT_SECRET,
+                {expiresIn: '1h'}
+            );
+
+            return res.status(200).json ({
+                message: 'Login bem-sucedido!',
+                token,
+                usuario: {
+                    email: usuario.email
+                }
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({message: 'Login mal-sucedido.'});
+        }
+    }
+}
