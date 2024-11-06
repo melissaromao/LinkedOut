6baea93d439ab4ae7ca77b5919d06e628a69ce67
melissaromao <melissarrdias@gmail.com>
2024-11-06 12:21:54 -0300

    Atualizando rotas, adicionando o authMiddlewares para proteger rotas e editar, excluir e listar usuário logado

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/cadastro', userController.cadastro);
+router.get('/usuario', authMiddleware, userController.getUserById);
+router.put('/editar', authMiddleware, userController.editar);
+router.delete('/excluir', authMiddleware, userController.excluir);
 
 module.exports = router;
