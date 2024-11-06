6a2d822633f57c735bcdb68cfae1402e2fb4d97c
melissaromao <melissarrdias@gmail.com>
2024-11-06 12:15:49 -0300

    Adicionando authMiddleware em middlewares para proteger as rotas e garantir que as operações em usuário só possam ser feitas pelo próprio usuário, verificando o token em cada requisição e associando ao id do usuário

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).json({message: 'Token não fornecido'});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.idUsuario = decoded.id;
+        next();
+    } catch (error) {
+        return res.status(401).json({message: 'Token inválido'});
+    }
+};
