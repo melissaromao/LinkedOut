c644a6fbd658dca34203669986757552e14eb4a7
melissaromao <melissarrdias@gmail.com>
2024-11-06 18:49:53 -0300

    Corrigindo rota protegida que retorna usuário logado de :id para :idUsuario

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/cadastro', userController.cadastro);
-router.get('/usuario', authMiddleware, userController.getUserById);
+router.get('/:idUsuario', authMiddleware, userController.getUserById);
 router.put('/editar', authMiddleware, userController.editar);
 router.delete('/excluir', authMiddleware, userController.excluir);
 
