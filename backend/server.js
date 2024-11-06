0f1350dcce47d6278bf29fc0b2ae233ee03d62f7
melissaromao <melissarrdias@gmail.com>
2024-11-06 11:01:58 -0300

    Adicionando body-parser para tratar corpo de requisições em json; Adicionando cors para que diferentes origens possam interagir com a API; Carregando variáveis de ambiente do arquivo .env; Definindo rotas para cadastro e autenticação

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 require('./models/relacoes');
-const sequelize = require('./config/database');
-const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const sequelize = require('./config/database');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
 
