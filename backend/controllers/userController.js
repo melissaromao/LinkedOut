382e4de61dcde9fe8110b375454e79b737d8e013
melissaromao <melissarrdias@gmail.com>
2024-11-06 12:09:15 -0300

    Adicionando função excluir para excluir usuário logado, verificando através do token JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,12 +4,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     cadastro: async (req, res) => {
-        const {email, senha} = req.body;
+        const { email, senha } = req.body;
 
         try {
-            const usuarioExistente = await Usuario.findOne({where: {email}});
+            const usuarioExistente = await Usuario.findOne({ where: { email } });
             if (usuarioExistente) {
-                return res.status(400).json({message: 'Email já cadastrado'});
+                return res.status(400).json({ message: 'Email já cadastrado' });
             }
 
             const senhaCriptografada = await bcrypt.hash(senha, 10);
@@ -21,21 +21,21 @@ module.exports = {
 
             return res.status(201).json({
                 message: 'Sucesso ao cadastrar usuário :D',
-                usuario: {id: novoUsuario.id, nome: novoUsuario.nome, email: novoUsuario.email},
+                usuario: { id: novoUsuario.id, nome: novoUsuario.nome, email: novoUsuario.email },
             });
         } catch (error) {
-            return res.status(500).json({message: 'Erro ao cadastrar usuário :('});
+            return res.status(500).json({ message: 'Erro ao cadastrar usuário :(' });
         }
     },
 
     getUserById: async (req, res) => {
-        const {idUsuario} = req;
+        const { idUsuario } = req;
 
         try {
             const usuario = await Usuario.findByPk(idUsuario);
 
             if (!usuario) {
-                return res.status(404).json({message: 'Usuário não encontrado'});
+                return res.status(404).json({ message: 'Usuário não encontrado' });
             }
 
             return res.status(200).json({
@@ -47,33 +47,53 @@ module.exports = {
             });
         } catch (error) {
             console.error(error);
-            return res.status(500).json({message: 'Erro ao buscar usuário'});
+            return res.status(500).json({ message: 'Erro ao buscar usuário' });
         }
     },
 
     editar: async (req, res) => {
-        const {idUsuario} = req;
-        const {email, senha} = req.body;
+        const { idUsuario } = req;
+        const { email, senha } = req.body;
 
         try {
             const usuario = await Usuario.findByPk(idUsuario);
-            
+
             if (!usuario) {
-                return res.status(404).json({message: 'Usuário não encontrado'});
+                return res.status(404).json({ message: 'Usuário não encontrado' });
             }
 
             usuario.email = email || usuario.email;
 
-            if(senha) {
+            if (senha) {
                 usuario.senha = await bcrypt.hash(senha, 10);
             }
 
             await usuario.save();
 
-            return res.status(200).json({message: 'Usuário atualizado com sucesso', usuario});
+            return res.status(200).json({ message: 'Usuário atualizado com sucesso', usuario });
         } catch (error) {
             console.error(error);
-            return res.status(500).json({message: 'Erro ao editar usuário'});
+            return res.status(500).json({ message: 'Erro ao editar usuário' });
         }
     },
+
+    excluir: async (req, res) => {
+        const { idUsuario } = req;
+        const { email, senha } = req.body;
+
+        try {
+            const usuario = await Usuario.findByPk(idUsuario);
+
+            if (!usuario) {
+                return res.status(404).json({ message: 'Usuário não encontrado' });
+            }
+
+            await usuario.destroy();
+
+            return res.status(200).json({ message: 'Usuário excluído com sucesso' });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: 'Erro ao excluir usuário' });
+        }
+    }
 };
