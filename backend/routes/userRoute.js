5cef962d923005b36c1f5e8b09554384ec14d221
melissaromao <melissarrdias@gmail.com>
2024-11-09 18:29:08 -0300

    Atualizando rota de listar usuário para perfil, não sendo necessário colocar o id do usuário na rota, agora só sendo necessário um token válido e atualizando o nome da função na rota cadastro para cadastrar, igual como está em userController.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/cadastro', userController.cadastro);
-router.get('/:idUsuario', authMiddleware, userController.getUserById);
+router.post('/cadastro', userController.cadastrar);
+router.get('/perfil', authMiddleware, userController.listar);
 router.put('/editar', authMiddleware, userController.editar);
 router.delete('/excluir', authMiddleware, userController.excluir);
 
