a9eb365f83f0bcbcf2d2dd331805e76c6be5eb08
melissaromao <melissarrdias@gmail.com>
2024-11-11 23:18:41 -0300

    Atualizando o status code que antes era devolvido em formato json para uma variável que armazena a mensagem é pode ser acessada e vista por meio do ejs, além disso a página é renderizada após o erro. Além disso, também criando um cookie chamado token, contendo o valor do jwt que é enviado para as próximas requisições http, sendo acessado apenas pelo servidor e não lido pelo cliente

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,29 +5,30 @@ const jwt = require('jsonwebtoken');
 module.exports = {
     login: async (req, res) => {
         const { email, senha } = req.body;
-
+    
         try {
             const usuario = await Usuario.findOne({ where: { email } });
-
+    
             if (!usuario) {
-                return res.status(404).json({ message: 'Usuário não encontrado' });
+                return res.render('index', { warning: 'Usuário não encontrado' });
             }
-
+    
             const senhaValida = await bcrypt.compare(senha, usuario.senha);
             if (!senhaValida) {
-                return res.status(400).json({ message: 'Senha incorreta' });
+                return res.render('index', { warning: 'Senha incorreta' });
             }
-
+    
             const token = jwt.sign(
                 { idUsuario: usuario.idUsuario },
                 process.env.JWT_SECRET,
                 { expiresIn: '1h' }
             );
-
-            return res.status(200).json({ message: 'Login bem-sucedido!', nome: usuario.nome, token });
+    
+            res.cookie('token', token, { httpOnly: true });
+            return res.render('home', { nome: usuario.nome });  // Página após login bem-sucedido
         } catch (error) {
             console.log(error);
-            return res.status(500).json({ message: 'Login mal-sucedido' });
+            return res.render('index', { error: 'Erro no Login' });
         }
     }
 }
