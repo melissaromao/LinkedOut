db64393ca0c543bb364686be786ef2ce9c5625e9
melissaromao <melissarrdias@gmail.com>
2024-11-13 09:00:27 -0300

    Adicionando rota de logout que limpa o cookie que armazena o token, redirecionando a página inicial para login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,30 +5,40 @@ const jwt = require('jsonwebtoken');
 module.exports = {
     login: async (req, res) => {
         const { email, senha } = req.body;
-    
+
         try {
             const usuario = await Usuario.findOne({ where: { email } });
-    
+
             if (!usuario) {
                 return res.render('index', { warning: 'Usuário não encontrado' });
             }
-    
+
             const senhaValida = await bcrypt.compare(senha, usuario.senha);
             if (!senhaValida) {
                 return res.render('index', { warning: 'Senha incorreta' });
             }
-    
+
             const token = jwt.sign(
                 { idUsuario: usuario.idUsuario },
                 process.env.JWT_SECRET,
                 { expiresIn: '1h' }
             );
-    
+
             res.cookie('token', token, { httpOnly: true });
-            return res.render('home', { nome: usuario.nome });  // Página após login bem-sucedido
+            return res.render('home', { usuario });
         } catch (error) {
             console.log(error);
             return res.render('index', { error: 'Erro no Login' });
+        }
+    },
+
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('token');
+            return res.render('index');
+
+        } catch (error) {
+            return res.render('home', { error: 'Erro ao sair' });
         }
     }
 }
