71e883cd4e65f25621fd69bdd7fbb675882afb98
melissaromao <melissarrdias@gmail.com>
2024-11-13 10:27:04 -0300

    Alterando rota editar para post permitindo atualizando via formulário  http

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,8 +4,8 @@ const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/cadastro', userController.cadastrar);
-router.get('/perfil', authMiddleware, userController.listar);
-router.put('/editar', authMiddleware, userController.editar);
+router.post('/perfil', authMiddleware, userController.listar);
+router.post('/editar', authMiddleware, userController.editar);
 router.delete('/excluir', authMiddleware, userController.excluir);
 
 module.exports = router;
