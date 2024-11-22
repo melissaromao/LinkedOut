3f14f7d7afc8dc6d491d23708eb7286394a22167
melissaromao <melissarrdias@gmail.com>
2024-11-22 13:27:51 -0300

    Adicionando rota de freela que pega o id da empresa para criar freela associado a empresa, rota de freelas, podendo ser acessada por todos, visualizando freelas, adicionando layout false em cadastro e index, adicionando ejs layouts

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 require('./models/relacoes');
-require('./models/Categoria');
 require('dotenv').config();
 
 const sequelize = require('./config/database');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8080;
 
@@ -21,33 +21,42 @@ app.use(cors());
 
 const authRoute = require('./routes/authRoute');
 const userRoute = require('./routes/userRoute');
-const categoriaRoute = require('./routes/categoriaRoute');
 const empresaRoute = require('./routes/empresaRoute');
+const freelaRoute  = require('./routes/freelaRoute')
 const authMiddleware = require('./middlewares/authMiddleware');
 const empresaController = require('./controllers/empresaController');
 const userController = require('./controllers/userController');
+const freelaController = require('./controllers/freelaController');
 
+app.use('/api/freela', freelaRoute);
 app.use('/api/empresa', empresaRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
-app.use('/api/categoria', categoriaRoute);
 app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(ejsLayouts);
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { layout: false });
 });
 
 app.get('/empresa/:idEmpresa', authMiddleware, empresaController.listar);
 
 app.get('/cadastro', (req, res) => {
-  res.render('cadastro');
+  res.render('cadastro', { layout: false });
 });
 
 app.get('/home', authMiddleware, userController.listar);
 
 app.get('/empresa', (req, res) => {
-  res.render('empresa');
+  res.render('empresa', {layout: false});
 });
+
+app.get('/empresa/:idEmpresa/freela', (req, res) => {
+  const idEmpresa = req.params.idEmpresa;
+  res.render('freela', { idEmpresa: idEmpresa });
+});
+
+app.get('/freelas', authMiddleware, freelaController.listarFreelas);
 
 sequelize.authenticate()
   .then(() => {
