d83cb02731cedb5235aa1405cc8e9286ca787192
melissaromao <melissarrdias@gmail.com>
2024-11-27 12:18:09 -0300

    Alterando lógica de filtro para integrar no controlador de listar freela

--- frontend/assets/js/filter.js
+++ frontend/assets/js/filter.js
@@ -1,20 +1,13 @@
-// Supondo que você tenha uma função para buscar as categorias
-fetch('http://localhost:8080/api/categoria/categoriaFindAll')
-  .then(response => response.json())
-  .then(data => {
-    console.log("Dados recebidos:", data);  // Confirme se os dados estão corretos
+document.getElementById('filter-category').addEventListener('change', function() {
+  const idCategoria = this.value;
+  const url = idCategoria ? `/freelas?page=1&categoriaId=${idCategoria}` : '/freelas?page=1';
 
-    // Selecionando o select pelo ID
-    const selectElement = document.getElementById("filter-category");
-
-    // Adicionando as opções dinamicamente
-    data.forEach(categoria => {
-      const option = document.createElement("option");
-      option.value = categoria.id;  // Supondo que o campo id exista
-      option.textContent = categoria.nome;  // E que o nome da categoria seja 'nome'
-      selectElement.appendChild(option);
-    });
-  })
-  .catch(error => {
-    console.error('Erro ao buscar categorias:', error);
-  });
+  fetch(url)
+      .then(response => response.text()) 
+      .then(html => {
+          document.getElementById('freelas-container').innerHTML = html; 
+      })
+      .catch(error => {
+          console.error('Erro ao aplicar o filtro de categoria:', error);
+      });
+});
